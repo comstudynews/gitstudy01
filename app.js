015d6da625bbd0f60e1ac399f0443be000afc61f
comstudy <comstudynews@gmail.com>
2024-05-27 14:21:37 +0900

    새로 수정

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.end('<h1>Hello world!</h1>');
 });
 
+app.get('/home', (req, res) => {
+    res.end('<h1>하하핳 하하</h1>');
+});
+
 const server = http.createServer(app);
 server.listen(3000, ()=>{
     console.log('Run on server http://localhost:3000');
