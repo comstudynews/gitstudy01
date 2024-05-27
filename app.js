63373ec2bfe993c81511038bbe19305ae09309ef
comstudy <comstudynews@gmail.com>
2024-05-27 14:14:13 +0900

    first

--- app.js
+++ app.js
@@ -6,14 +6,6 @@ app.get('/', (req, res) => {
     res.end('<h1>Hello world!</h1>');
 });
 
-app.get('/home', (req, res) => {
-    res.end('<h1>Hello world!</h1>');
-});
-
-app.get('/profile', (req, res) => {
-    res.end('<h1>Hello world!</h1>');
-});
-
 const server = http.createServer(app);
 server.listen(3000, ()=>{
     console.log('Run on server http://localhost:3000');
